b6418f56f50ac1609422bed3ad3f91fb8a6affe3
nklyatyshev <nklyatyshev@mmtr.tu>
2022-03-05 16:07:24 +0300

    add new strategy

---
+++ src/main/java/src/main/java/vc/branches/manag/services/HelloCaseService.java
@@ -0,0 +1,8 @@
+package src.main.java.vc.branches.manag.services;
+
+public class HelloCaseService implements Strategy {
+
+    public void sayHello(){
+        System.out.println("HELLO!");
+    }
+}
