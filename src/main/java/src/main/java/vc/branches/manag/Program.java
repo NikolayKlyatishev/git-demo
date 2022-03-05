c392433c0dfd3c5b4cef9ef99121a57aa7cc6e8a
Nick Kl <32437121+NikolayKlyatishev@users.noreply.github.com>
2022-03-05 16:14:25 +0300

    feature/2 (#2)
    * add new strategy
    
    * fix added strategy from start application
    
    * fix message
    
    Co-authored-by: nklyatyshev <nklyatyshev@mmtr.tu>

--- src/main/java/src/main/java/vc/branches/manag/Program.java
+++ src/main/java/src/main/java/vc/branches/manag/Program.java
@@ -2,14 +2,18 @@ package src.main.java.vc.branches.manag;
 
 import src.main.java.vc.branches.manag.services.Strategy;
 
-public class Program {
-    private final Strategy helloService;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    public Program(Strategy helloService) {
-        this.helloService = helloService;
+public class Program {
+    private final List<Strategy> helloService;
+
+    public Program(Strategy ... helloService) {
+        this.helloService = Arrays.stream(helloService).collect(Collectors.toList());
     }
 
     public void start() {
-        helloService.sayHello();
+        helloService.forEach(Strategy::sayHello);
     }
 }
