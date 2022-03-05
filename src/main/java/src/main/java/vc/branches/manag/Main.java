26d045ee26f94f8e1ee3d0991d0883cc2f4c767b
nklyatyshev <nklyatyshev@mmtr.tu>
2022-03-05 16:12:28 +0300

    fix added strategy from start application

--- src/main/java/src/main/java/vc/branches/manag/Main.java
+++ src/main/java/src/main/java/vc/branches/manag/Main.java
@@ -1,12 +1,12 @@
 package src.main.java.vc.branches.manag;
 
+import src.main.java.vc.branches.manag.services.HelloCaseService;
 import src.main.java.vc.branches.manag.services.HelloService;
 import src.main.java.vc.branches.manag.services.Strategy;
 
 public class Main {
     public static void main(String[] args) {
-        Strategy hello = new HelloService();
-        var program = new Program(hello);
+        var program = new Program(new HelloService(), new HelloCaseService());
         program.start();
     }
 }
